0c2113720af28363c53928aaa15528ce309bcd4b
Austin Wise <AustinWise@gmail.com>
2023-05-30 20:54:17 -0700

    Basic custom native main sample.

---
+++ src/CustomMain/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    [UnmanagedCallersOnly(EntryPoint = "PreMainFunction", CallConvs = new Type[] { typeof(CallConvCdecl) })]
+    static void PreMainFunction(int amount)
+    {
+        Console.WriteLine($"Hello from PreMainFunction, got {amount}");
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"In managed main, got {args.Length} args:");
+        for (int i = 0; i < args.Length; i++)
+        {
+            Console.WriteLine($"args[{i}]: {args[i]}");
+        }
+    }
+}
